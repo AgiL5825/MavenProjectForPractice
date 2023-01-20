58d106693c51ce12a464bf9e8a44467dfe219549
19786 <agil83p@yahoo,com>
2023-01-20 12:02:29 -0500

    New Commit to Github

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello test");
         System.out.println("New commit");
+        System.out.println("message for Github");
+        System.out.println("Doing more works");
     }
 }
